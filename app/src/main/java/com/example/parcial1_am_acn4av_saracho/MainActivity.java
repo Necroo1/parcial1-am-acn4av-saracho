449abc6fb22c961d714a7db5f50da871f57c945a
Necroo1 <michaeldavidsg@gmail.com>
2025-05-05 21:48:51 -0300

    feat: Implement rounded corners for the dynamic "+" button

--- app/src/main/java/com/example/parcial1_am_acn4av_saracho/MainActivity.java
+++ app/src/main/java/com/example/parcial1_am_acn4av_saracho/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.parcial1_am_acn4av_saracho;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,11 +19,13 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private TextView selectedCategoryTitle;
+    private LinearLayout addButtonContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addButtonContainer = findViewById(R.id.addButtonContainer);
         selectedCategoryTitle = findViewById(R.id.selected_category_title);
         Button fruitsButton = findViewById(R.id.category_fruits_button);
         Button vegetablesButton = findViewById(R.id.category_vegetables_button);
@@ -57,6 +64,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 selectedCategoryTitle.setText(R.string.category_red_meats);
+            }
+        });
+//BOTON +
+        Button addButton = new Button(this);
+        addButton.setText("+");
+        addButton.setTextSize(24);
+        addButton.setTextColor(getResources().getColor(android.R.color.white));
+        addButton.setGravity(Gravity.CENTER);
+        GradientDrawable roundedBackground = new GradientDrawable();
+        roundedBackground.setShape(GradientDrawable.RECTANGLE);
+        roundedBackground.setColor(Color.BLACK);
+        roundedBackground.setCornerRadius(400f);
+        addButton.setBackground(roundedBackground);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        addButtonContainer.addView(addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Aquí iría la lógica para abrir la pantalla de carga de ingredientes
+                Log.d("MainActivity", "Botón '+' clickeado");
             }
         });
     }
