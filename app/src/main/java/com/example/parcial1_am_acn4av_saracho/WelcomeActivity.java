6308a33d5821ea9a07e4f7435dda9f119ad9b153
Necroo1 <michaeldavidsg@gmail.com>
2025-07-01 18:22:32 -0300

    feat: add welcome page

---
+++ app/src/main/java/com/example/parcial1_am_acn4av_saracho/WelcomeActivity.java
@@ -0,0 +1,22 @@
+package com.example.parcial1_am_acn4av_saracho;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button btnComencemos = findViewById(R.id.comencemos);
+        btnComencemos.setOnClickListener(v -> {
+            Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
